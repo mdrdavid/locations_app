1d8f9ba150c9e0516c64051f110ff10e94038a36
david <davidmatovu88@gmail.com>
2023-01-25 08:48:19 +0300

    add custom hook

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -6,6 +6,7 @@ import {
 	VALIDATOR_REQUIRE,
 	VALIDATOR_MINLENGTH,
 } from "../../shared/utils/validators";
+import { useForm } from "../../shared/hooks/form-hook";
 import "./PlaceForm.css";
 
 const formReducer = (state, action) => {
