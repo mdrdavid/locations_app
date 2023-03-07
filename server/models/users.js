4b823c2a2186be9dfb17ad4c0bec463a709ca30c
david <davidmatovu88@gmail.com>
2023-03-07 08:24:14 +0300

    add mongoose unique validator to vslidate email

--- server/models/users.js
+++ server/models/users.js
@@ -1,11 +1,15 @@
-const mongoose = require("mongoose")
-const uniqueValidator = require("mongoose-unique-validator")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: {type:String, required: true},
-  email: {type:String, required: true, unique: true},
-  password: {type:String, required: true, minLength: 6},
-  image: {type:String, required: true},
-  places: {type:String, required: true},
-})
+	name: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true, minLength: 6 },
+	image: { type: String, required: true },
+	places: { type: String, required: true },
+});
+// make sure the email is unique
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
