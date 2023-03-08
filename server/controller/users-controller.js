de1da1983aca36ba554d86dee295525849af3780
david <davidmatovu88@gmail.com>
2023-03-08 16:26:38 +0300

    add user login

--- server/controller/users-controller.js
+++ server/controller/users-controller.js
@@ -72,15 +72,24 @@ const signup = async (req, res, next) => {
 	res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
 	const { email, password } = req.body;
-	const identifiedUser = DUMMY_USERS.find((user) => user.email === email);
-	if (!identifiedUser || identifiedUser.password !== password) {
-		throw new HttpError(
-			"Could not identify user, credentials seem to be wrong",
-			401
-		); // 401 means authentication failed
+	let existingUser;
+	try {
+		existingUser = await User.findOne({ email: email });
+	} catch (err) {
+		const error = new HttpError("Login failed, Please try again later", 500);
+		return next(error);
 	}
+
+	if (!existingUser || existingUser.password !== password) {
+		const error = new HttpError(
+			"Invalid credentials, could not log you in",
+			401
+		);
+		return next(error);
+	}
+
 	res.json({ message: "logged in" });
 };
 
