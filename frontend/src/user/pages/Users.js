aec8f895c897fe41b0b2251544c6a4d6a7a03d53
david <davidmatovu88@gmail.com>
2023-03-16 18:16:44 +0300

    fetch users

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,12 +1,56 @@
-import { UsersList } from "../components/UsersList";
+import React, { useEffect, useState } from 'react'
+import { UsersList } from '../components/UsersList'
+import ErrorModal from '../../shared/UIElements/ErrorModal'
+import LoadingSpinner from '../../shared/UIElements/LoadingSpinner'
+
 export const Users = () => {
-	const Users = [
-		{
-			id: "u1",
-			name: "David Matovu",
-			places: 2,
-			image: "https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300",
-		},
-	];
-	return <UsersList items={Users}/>;
-};
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState()
+    const [loadedUsers, setLoadedUsers] = useState()
+    useEffect(() => {
+        const sendRequest = async () => {
+            setIsLoading(true)
+            try {
+                const response = await fetch('http://localhost:5000/api/users')
+
+                const responseData = await response.json()
+
+                if (!response.ok) {
+                    throw new Error(responseData.message)
+                }
+                setLoadedUsers(responseData.users)
+
+                // setIsLoading(false)
+            } catch (err) {
+                setError(err.message)
+            }
+            setIsLoading(false)
+        }
+        sendRequest()
+    }, [])
+
+    const errorHandler = () => {
+        setError(null)
+    }
+    // const Users = [
+    // 	{
+    // 		id: "u1",
+    // 		name: "David Matovu",
+    // 		places: 2,
+    // 		image: "https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300",
+    // 	},
+    // ];
+    // return <UsersList items={Users} />
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={errorHandler} />
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner asOverLay />
+                </div>
+            )}
+            {/* don't render if loaded users is undefined */}
+            {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+        </React.Fragment>
+    )
+}
